f9c76cee35f4be935d507499a2466d27b6aacfcb
Paolo <maciuozz@hotmail.it>
2023-04-27 15:26:00 +0200

    fix(app): fixed code errors

---
+++ ci-utils/update_versions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+NEXT_RELEASE_VERSION="${1#"v"}"
+
+function update_version_helm_chart_files() {
+  echo "It's going to update appVersion in helm chart to version ${NEXT_RELEASE_VERSION}"
+  yq w -i helm/Chart.yaml "appVersion" "${NEXT_RELEASE_VERSION}"
+
+  echo "It's going to update version in helm chart to version ${NEXT_RELEASE_VERSION}"
+  yq w -i helm/Chart.yaml "version" "${NEXT_RELEASE_VERSION}"
+
+  echo "It's going to update image tag in helm chart values to version ${NEXT_RELEASE_VERSION}"
+  yq w -i helm/values.yaml "image.tag" "${NEXT_RELEASE_VERSION}"
+}
+
+function update_helm_docs() {
+  echo "It's going to update README.md with helm-docs"
+  helm-docs -c helm/
+}
+
+function update_versions() {
+  update_version_helm_chart_files
+  update_helm_docs
+}
+
+update_versions
